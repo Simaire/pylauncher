562d8ee7043f007df88b220254a09bb6f5add597
Simaire <52448691+Simaire@users.noreply.github.com>
2022-06-07 18:41:21 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+import json
+import os
+import time
+
+def lunch():
+
+    global config
+
+    with open('config.json', 'r') as file:
+        config = json.load(file)
+        file.close()
+
+
+def save(ver, var, defi):
+
+    if ver != "":
+        config[ver][var] = defi
+
+    with open('config.json', 'w') as file:
+        json.dump(config, file, sort_keys=True, indent=4)
+        file.close()
+
+
+
+if config["0"]["active"] == False:
+
+    lunch()
+    save("0", "version", "0.1.2")
+
+    print("Initialisation terminer de Main.py")
+else:
+    print("cc2")
