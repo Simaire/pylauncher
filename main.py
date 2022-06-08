4ea31e29bbd2119fe3323c5ae36775bd39ae0665
Simaire <52448691+Simaire@users.noreply.github.com>
2022-06-08 16:18:52 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ import json
 import os
 import time
 
-def lunch(id):
+global config
+global active
+global key
+global name
+global name_link
+global version
+global version_file
+global version_link
 
+
+def lunch(fid):
     global config
 
     global active
@@ -18,16 +27,16 @@ def lunch(id):
         config = json.load(file)
         file.close()
 
-    active = config[id]["active"]
-    key = config[id]["key"]
-    name = config[id]["name"]
-    name_link = config[id]["name_link"]
-    version = config[id]["version"]
-    version_file = config[id]["version_file"]
-    version_link = config[id]["version_link"]
+    active = config[fid]["active"]
+    key = config[fid]["key"]
+    name = config[fid]["name"]
+    name_link = config[fid]["name_link"]
+    version = config[fid]["version"]
+    version_file = config[fid]["version_file"]
+    version_link = config[fid]["version_link"]
 
-def save(id):
 
+def save(fid):
     global config
 
     global active
@@ -42,18 +51,16 @@ def save(id):
         json.dump(config, file, sort_keys=True, indent=4)
         file.close()
 
-    config[id]["active"] =active
-    config[id]["key"] = key
-    config[id]["name"] = name
-    config[id]["name_link"] = name_link
-    config[id]["version"] = version
-    config[id]["version_file"] = version_file
-    config[id]["version_link"] = version_link
-
+    config[fid]["active"] = active
+    config[fid]["key"] = key
+    config[fid]["name"] = name
+    config[fid]["name_link"] = name_link
+    config[fid]["version"] = version
+    config[fid]["version_file"] = version_file
+    config[fid]["version_link"] = version_link
 
 
 if config["0"]["active"] == False:
-
 
     lunch("0")
     version = "0.1.0"
